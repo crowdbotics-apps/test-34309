d7320ae03e1ba338fe794186ccab1ef4f71dbcb0
Crowdbotics <team@crowdbotics.com>
2022-03-21 09:27:51 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SubUser
+
+admin.site.register(SubUser)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+
+
+class SubUser(models.Model):
+    "Generated Model"
+    surname = models.CharField(
+        max_length=256,
+    )
